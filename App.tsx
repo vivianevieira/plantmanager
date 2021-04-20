d435e2b0d51afc2c6fefaff73774ca0c65d16c66
Viviane Vieira <viviane.kodama@gmail.com>
2021-04-20 12:20:54 -0700

    Started creating UserIdentication page.

--- App.tsx
+++ App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { useFonts, Jost_400Regular, Jost_600SemiBold } from '@expo-google-fonts/jost';
 import Apploading from 'expo-app-loading';
 
-import { Welcome } from './src/pages/Welcome';
+import { UserIdentification } from './src/pages/UserIdentification';
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -15,6 +15,6 @@ export default function App() {
     }
 
   return (
-    <Welcome />
+    <UserIdentification />
   );
 }
