dab3479f2086ac93b52f0f77e6c8d72a25d71630
Viviane Vieira <viviane.kodama@gmail.com>
2021-04-20 12:55:53 -0700

    Styled emoji and input element according to state of user name being typed.

--- src/pages/UserIdentification.tsx
+++ src/pages/UserIdentification.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
   SafeAreaView,
   StyleSheet,
@@ -15,6 +15,24 @@ import colors from '../styles/colors';
 import fonts from '../styles/fonts';
 
 export function UserIdentification() {
+  const [isFocused, setIsFocused] = useState(false);
+  const [isFilled, setIsFilled] = useState(false);
+  const [name, setName] = useState<string>();
+
+  function handleInputBlur() {
+    setIsFocused(false);
+    setIsFilled(!!name);
+  }
+
+  function handleInputFocus() {
+    setIsFocused(true);
+  }
+
+  function handleInputChange(value: string) {
+    setIsFilled(!!value);
+    setName(value);
+  }
+
   return (
     <SafeAreaView style={styles.container}>
       <KeyboardAvoidingView
@@ -25,7 +43,7 @@ export function UserIdentification() {
           <View style={styles.form}>
             <View style={styles.header}>
               <Text style={styles.emoji}>
-                😀
+                {isFilled ? '😄' : '😀'}
               </Text>
 
               <Text style={styles.title}>
@@ -34,7 +52,17 @@ export function UserIdentification() {
               </Text>
             </View>
 
-            <TextInput style={styles.input} placeholder="Digite um nome" />
+            <TextInput
+              style={[
+                styles.input,
+                (isFocused || isFilled) &&
+                { borderColor: colors.green }
+              ]}
+              placeholder="Digite um nome"
+              onBlur={handleInputBlur}
+              onFocus={handleInputFocus}
+              onChangeText={handleInputChange}
+            />
 
             <View style={styles.footer}>
               <Button />
