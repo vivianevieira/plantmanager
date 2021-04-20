baf62a93f55c43fdd068944386b704f9425fff83
Viviane Vieira <viviane.kodama@gmail.com>
2021-04-20 11:59:21 -0700

    Imported fonts and created a sperate style for font styles.

--- App.tsx
+++ App.tsx
@@ -1,7 +1,19 @@
 import React from 'react';
+import { useFonts, Jost_400Regular, Jost_600SemiBold } from '@expo-google-fonts/jost';
+import Apploading from 'expo-app-loading';
+
 import { Welcome } from './src/pages/Welcome';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Jost_400Regular,
+    Jost_600SemiBold
+  });
+
+    if (!fontsLoaded) {
+      return <Apploading />
+    }
+
   return (
     <Welcome />
   );
