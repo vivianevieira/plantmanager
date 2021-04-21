4217c098834a91bb2ba0f603501eae4af5e3e523
Viviane Vieira <viviane.kodama@gmail.com>
2021-04-21 11:19:49 -0700

    Created Header component for PlantSelect page and Buttons.

--- src/pages/PlantSelect.tsx
+++ src/pages/PlantSelect.tsx
@@ -5,12 +5,29 @@ import {
   StyleSheet
 } from 'react-native';
 
+import { EnvironmentButton } from '../components/EnvironmentButton';
+import { Header } from '../components/Header';
+
 import colors from '../styles/colors';
+import fonts from '../styles/fonts';
 
 export function PlantSelect() {
   return (
     <View style={styles.container}>
-      <Text>Selecionar Planta</Text>
+
+      <View style={styles.header}>
+        <Header />
+
+        <Text style={styles.title}>
+          Em qual ambiente
+        </Text>
+        <Text style={styles.subtitle}>
+          você quer colocar sua planta?
+        </Text>
+      </View>
+
+      <EnvironmentButton title="Cozinha" active/>
+
     </View>
   );
 }
@@ -18,8 +35,23 @@ export function PlantSelect() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
     backgroundColor: colors.background
   },
+  header: {
+    paddingHorizontal: 30
+  },
+  title: {
+    fontSize: 17,
+    color: colors.heading,
+    fontFamily: fonts.heading,
+    lineHeight: 20,
+    marginTop: 15
+  },
+  subtitle: {
+    fontFamily: fonts.text,
+    fontSize: 17,
+    lineHeight: 20,
+    color: colors.heading
+
+  }
 });
