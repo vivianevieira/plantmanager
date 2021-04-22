192e930c055fa7aa400ae54e83fef1f9a34eb1d7
Viviane Vieira <viviane.kodama@gmail.com>
2021-04-22 11:29:56 -0700

    Passed plant info as param to PlantSave page.

--- src/pages/PlantSave.tsx
+++ src/pages/PlantSave.tsx
@@ -9,28 +9,49 @@ import {
   Platform,
   TouchableOpacity
 } from 'react-native';
+
+import { getBottomSpace } from 'react-native-iphone-x-helper';
 import { SvgFromUri } from 'react-native-svg';
+import { useRoute } from '@react-navigation/core';
 
 import waterDropImg from'../assets/waterdrop.png'
 import { Button } from '../components/Button';
 import colors from '../styles/colors';
+import fonts from '../styles/fonts';
+
+interface Params {
+  plant: {
+    id: string;
+    name: string;
+    about: string;
+    water_tips: string;
+    photo: string;
+    environments: [string];
+    frequency: {
+      times: number;
+      repeat_every: string;
+    }
+  }
+}
 
 export function PlantSave() {
+  const route = useRoute();
+  const { plant } = route.params as Params;
+
   return (
     <View style={styles.container}>
       <View style={styles.plantInfo}>
         <SvgFromUri
-          uri=""
+          uri={plant.photo}
           height={150}
           width={150}
         />
 
         <Text style={styles.plantName}>
-          Nome da planta
+          {plant.name}
         </Text>
         <Text style={styles.plantAbout}>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit.
-          Aspernatur esse explicabo nemo nesciunt quis provident.
+          {plant.about}
         </Text>
       </View>
       <View style={styles.controller}>
@@ -40,7 +61,7 @@ export function PlantSave() {
             style={styles.tipImage}
           />
           <Text style={styles.tipText}>
-            Lorem ipsum dolor sit amet.
+            {plant.water_tips}
           </Text>
         </View>
 
@@ -66,16 +87,64 @@ const styles = StyleSheet.create({
 
   },
   plantInfo: {
-
+    flex: 1,
+    paddingHorizontal: 30,
+    paddingVertical: 50,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: colors.shape
   },
   plantName: {
+    fontFamily: fonts.heading,
+    fontSize: 24,
+    color: colors.heading,
+    marginTop: 15
 
   },
   plantAbout: {
+    textAlign: 'center',
+    fontFamily: fonts.text,
+    color: colors.heading,
+    fontSize: 17,
+    marginTop: 10
 
   },
   controller: {
+    backgroundColor: colors.white,
+    paddingHorizontal: 20,
+    paddingTop: 20,
+    paddingBottom: getBottomSpace() || 20
 
+  },
+  tipContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    backgroundColor: colors.blue_light,
+    padding: 20,
+    borderRadius: 20,
+    position: 'relative',
+    bottom: 60
+  },
+  tipImage: {
+    width: 56,
+    height: 56
+  },
+  tipText: {
+    flex: 1,
+    marginLeft: 20,
+    fontFamily: fonts.text,
+    color: colors.blue,
+    fontSize: 17,
+    textAlign: 'justify'
+  },
+  alertLabel: {
+    textAlign: 'center',
+    fontFamily: fonts.complement,
+    color: colors.heading,
+    fontSize: 12,
+    marginBottom: 5
   }
+
 
 });
