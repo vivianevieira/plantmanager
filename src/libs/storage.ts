c9152b1fbcebaaf20e96ef7bfd6e39e878a11407
Viviane Vieira <viviane.kodama@gmail.com>
2021-04-23 12:03:19 -0700

    Added missing hour prop to interface PlantProps and exported interface StoragePlantProps .

--- src/libs/storage.ts
+++ src/libs/storage.ts
@@ -11,11 +11,12 @@ export interface PlantProps {
   frequency: {
     times: number;
     repeat_every: string;
-  },
+  };
+  hour: string;
   dateTimeNotification: Date;
 }
 
-interface StoragePlantProps {
+export interface StoragePlantProps {
   [id: string]: {
     data: PlantProps;
   }
